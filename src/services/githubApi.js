5d4c93969085ec55d06d4e36ad7f81391f6c2d9f
Denis Mludek <denis.mludek@supinfo.com>
2016-02-25 12:49:58 +0100

    refactor githubApi and add functions getCommiters and getContributors

--- src/services/githubApi.js
+++ src/services/githubApi.js
@@ -2,24 +2,43 @@ import fetchPlus from 'fetch-plus'
 import plusJson from 'fetch-plus-json'
 
 const URL_API = 'https://api.github.com'
-const SEARCH_REPOS_ENDPOINT = '/search/repositories'
-const GET_REPO_ENDPOINT = '/repos/:owner/:repo'
+const SEARCH_REPOS_URI = 'search/repositories'
+const REPOS_URI = 'repos'
+const CONTRIBUTORS_URI = 'contributors'
+const COMMITS_URI = 'commits'
 
-const endpoint = fetchPlus.connectEndpoint(URL_API)
-endpoint.addMiddleware(plusJson())
+const endpoint = fetchPlus.connectEndpoint(URL_API).addMiddleware(plusJson())
+
+/* Requests that return multiple items will be paginated to 30 items by default.
+  You can specify further pages with the ?page parameter.
+  For some resources, you can also set a custom page size up to 100 with the ?per_page parameter.
+  Note that for technical reasons not all endpoints respect the ?per_page parameter, see events for example.*/
 
 export const githubApi = {
-
-  searchInRepositories(q, per_page = 15) {
+  searchInRepositories: (q, per_page = 15) => {
     return endpoint.browse(
-      SEARCH_REPOS_ENDPOINT,
-      { query: { q, per_page } }
+      SEARCH_REPOS_URI,
+      {query: {q, per_page}}
     )
   },
 
-  getRepository(owner, repo) {
-    const uri = GET_REPO_ENDPOINT.replace(':owner', owner).replace(':repo', repo)
-    return endpoint.browse(uri)
+  getRepository: (owner, repo) => {
+    return endpoint.browse(
+      [REPOS_URI, owner, repo]
+    )
+  },
+
+  getContributors: (owner, repo, per_page = 20) => {
+    return endpoint.browse(
+      [REPOS_URI, owner, repo, CONTRIBUTORS_URI],
+      {query: {per_page}}
+    )
+  },
+
+  getCommits: (owner, repo, per_page = 100) => {
+    return endpoint.browse(
+      [CONTRIBUTORS_URI, owner, repo, COMMITS_URI],
+      {query: {per_page}}
+    )
   }
 }
-
