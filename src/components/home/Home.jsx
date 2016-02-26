fe57cf700b46fb68e54cc29e72f4c95737378ccf
Denis Mludek <denis.mludek@supinfo.com>
2016-02-26 16:27:43 +0100

    Home: refactor to functionnal stateless component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,15 +2,12 @@ import React, { Component } from 'react'
 
 import SearchBar from '../searchbar/SearchBar'
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div className="inner cover">
-        <h1 classNameName="cover-heading">Search repositories</h1>
-
-        <SearchBar />
-
-      </div>
-    )
-  }
+const Home = () => {
+  return (
+    <div className="home">
+      <SearchBar />
+    </div>
+  )
 }
+
+export default Home
