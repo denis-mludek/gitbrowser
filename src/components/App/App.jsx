1c89fd283922df34aab71455541e970779828359
Denis Mludek <denis.mludek@supinfo.com>
2016-02-26 16:29:58 +0100

    App: refactor to functionnal component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,20 +5,15 @@ import Header from '../header/Header'
 
 import './App.sass'
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="site-wrapper">
-        <div className="site-wrapper-inner">
-          <div className="cover-container">
-
-            <Header />
-
-            {this.props.children}
-
-          </div>
-        </div>
+const App = ({children}) => {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        {children}
       </div>
-    );
-  }
+    </div>
+  )
 }
+
+export default App
