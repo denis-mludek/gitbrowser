8f2e68000cafd3af173853a7b8ecba696e22b333
Denis Mludek <denis.mludek@supinfo.com>
2016-02-26 16:18:37 +0100

    change body Header to functionnal stateless component and css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,19 +2,27 @@ import React, { Component } from 'react'
 
 import NavLink from './NavLink'
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div className="masthead clearfix">
-        <div className="inner">
-          <h3 className="masthead-brand">GitBrowser</h3>
-          <nav>
-            <ul className="nav masthead-nav">
-              <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
-            </ul>
-          </nav>
+const Header = () =>  {
+  return (
+    <nav className="navbar navbar-inverse navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="#">GitBrowser</a>
+        </div>
+        <div className="collapse navbar-collapse">
+          <ul className="nav navbar-nav navbar-right">
+            <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
+          </ul>
         </div>
       </div>
-    );
-  }
+    </nav>
+  )
 }
+
+export default Header
