6ca65e67bd9fffa01dfa6cd954dda0ceed122bab
Denis Mludek <denis.mludek@supinfo.com>
2016-02-28 13:46:22 +0100

    refactor(githubApi): custom getContributors with pagination

--- src/services/githubApi.js
+++ src/services/githubApi.js
@@ -1,10 +1,10 @@
 import fetchPlus from 'fetch-plus'
 import plusJson from 'fetch-plus-json'
+import parseLinkHeader from 'parse-link-header'
 
 const URL_API = 'https://api.github.com'
 const SEARCH_REPOS_URI = 'search/repositories'
 const REPOS_URI = 'repos'
-const CONTRIBUTORS_URI = 'contributors'
 const COMMITS_URI = 'commits'
 
 const endpoint = fetchPlus.connectEndpoint(URL_API).addMiddleware(plusJson())
@@ -15,27 +15,29 @@ const endpoint = fetchPlus.connectEndpoint(URL_API).addMiddleware(plusJson())
   Note that for technical reasons not all endpoints respect the ?per_page parameter, see events for example.*/
 
 export const githubApi = {
-  searchInRepositories: (q, per_page = 15) => {
+  searchInRepositories(q, per_page = 15) {
     return endpoint.browse(
       SEARCH_REPOS_URI,
       {query: {q, per_page}}
     )
   },
 
-  getRepository: (owner, repo) => {
+  getRepository(owner, repo) {
     return endpoint.browse(
       [REPOS_URI, owner, repo]
     )
   },
 
-  getContributors: (owner, repo, per_page = 20) => {
-    return endpoint.browse(
-      [REPOS_URI, owner, repo, CONTRIBUTORS_URI],
-      {query: {per_page}}
-    )
+  async getContributors(urlEndpoint, page, per_page) {
+    const response = await fetchPlus.fetch(urlEndpoint, {query: {per_page, page}})
+    const parsedPagination = parseLinkHeader(response.headers.get('Link'))
+    const jsonBody = await response.json()
+    const mergedJsonPagination = Object.assign({}, {contributors:jsonBody, pagination: parsedPagination})
+
+    return mergedJsonPagination
   },
 
-  getCommits: (owner, repo, per_page = 100) => {
+  getCommits(owner, repo, per_page = 100) {
     return endpoint.browse(
       [CONTRIBUTORS_URI, owner, repo, COMMITS_URI],
       {query: {per_page}}
