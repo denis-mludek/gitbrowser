19a69d2e9178f275436b94b8d26eacf90aa8084b
Denis Mludek <denis.mludek@supinfo.com>
2016-03-06 15:58:48 +0100

    fix(searchbar): display nb results even if 0

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -32,6 +32,7 @@ export default class SearchBar extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
+    console.log('HERE')
     this.setState({isOpen: true})
   }
 
@@ -40,16 +41,12 @@ export default class SearchBar extends Component {
   }
 
   _onChange = (event) => {
-    this.setState({text: event.target.value, isOpen: false})
     const text = event.target.value
+    this.setState({text, isOpen: false})
 
-  if(text.length >= MIN_CHARS) {
+    if(text.length >= MIN_CHARS) {
       this.fetchRepos(text)
     }
-  }
-
-  secureInput(input) {
-    return encodeURIComponent(input.trim())
   }
 
   _onBlur = () => {
