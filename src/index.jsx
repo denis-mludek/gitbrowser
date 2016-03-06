f1954b5f0d2fd76ec2dfa7d4197a9c3aaf9c63a3
Denis Mludek <denis.mludek@supinfo.com>
2016-03-06 15:52:51 +0100

    feat: handle route error

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,14 @@ import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './components/app/App'
 import RepositoryContainer from './components/repository/containers/RepositoryContainer'
 import Home from './components/home/Home'
+import NoMatchRoute from './components/noMatchRoute/NoMatchRoute'
 
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home}/>
       <Route path="/repos/:userName/:repoName" component={RepositoryContainer}/>
+      <Route path="*" component={NoMatchRoute}/>
     </Route>
   </Router>
 ), document.getElementById('root'))
